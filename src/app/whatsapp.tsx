ac4d780d05ef9c23c8886998a6852674d1a4612e
Affankha <khanaffan34163@gmail.com>
2025-05-10 10:31:26 +0530

    all files are added

---
+++ src/app/whatsapp.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Link } from "lucide-react";
+
+export default function WhattsApp(){
+  
+  return(<>
+  <Link href="" className="z-50 absolute ml-10 mb-10 ">
+    <Image src="" height={200} width={200} alt="/whatsapp.png" className="fixed"></Image>
+  </Link>
+  </>);
+}
